6307555aaa8142126041bc22b96cc11d61c8b96d
Adeh Desandies <adeh@adehmac.local>
2009-07-21 18:52:53 +0800

    refactored to get all info

--- SVNHelper.rb
+++ SVNHelper.rb
@@ -1,28 +1,58 @@
-SVN_FILE = ".svn/entries"
+
 class SVNHelper
   
- attr_accessor :version, :date
+  attr_accessor :revision, :date, :last_changed, :info
  
- def initialize(version, date)
-   @version  = version 
-   @date     = date
- end
- alias rev version 
-
- def self.version(root)
-   file = File.join(root, SVN_FILE)  
-   raise "Specified SVN file [#{file}] does not exist, can not get the revision information."  unless File.exists?(file)
+  def initialize(info)
+    @revision         = info[:revision] 
+    @date             = Time.parse(info[:last_changed_date])
+    @last_changed     = info[:last_changed_rev]
+    @info             = info
+  end
+  alias rev revision
   
-  #third line is the revision name, ninth line is the last modified date.
-   begin
-     lines   = IO.readlines(file)
-     version = self.new(lines[3].chomp,         
-                        lines[9].chomp.to_time)
-   rescue
-    #in case of a change of SVN kernel, just return 'no_svn' and we will look back here.
-    version = self.new('NO_SVN',Date.today)     
-   end 
-   version
+ #  mac$ svn info
+ #  Path: .
+ #  URL: svn+ssh://server/path/to/repo
+ #  Repository Root: svn+ssh://server/code/repo
+ #  Repository UUID: 92465c46-2311-0410-8e48-c621c597944e
+ #  Revision: 6864
+ #  Node Kind: directory
+ #  Schedule: normal
+ #  Last Changed Author: wsk
+ #  Last Changed Rev: 6845
+ #  Last Changed Date: 2009-07-17 11:02:20 +0800 (Fri, 17 Jul 2009)
+ #  
+ 
+  MARKERS = {
+    :url                  => 'URL',
+    :repository_root      => 'Repository Root',
+    :repository_uuid      => 'Repository UUID',
+    :revision             => 'Revision',
+    :node_kind            => 'Node Kind',
+    :schedule             => 'Schedule',
+    :last_changed_author  => 'Last Changed Author',
+    :last_changed_rev     => 'Last Changed Rev',
+    :last_changed_date    => 'Last Changed Date'
+  }
+  
+  def self.version(root)
+    raise "Specified Application root: #{root} does not exist, can not get the revision information." unless File.exists?(root)
+    begin 
+      #get the return result of command: svn info
+      svn_info = %x{svn info #{root}} 
+      info = {}
+      MARKERS.each do |name, mark| 
+        line = svn_info.match(/^#{mark}:\s(.*)/)
+        info[name] = line[1]
+      end
+      version = self.new(info) 
+    rescue
+      # in case of a change of svn info command or the application is not using svn, just return 'no_svn' and we will look back here.
+      version = self.new({:revision=>'NO_SVN'})     
+    end
+   
+   version.freeze
    
  end
  
@@ -30,7 +60,7 @@ class SVNHelper
    rev
  end
  def rev_with_date
-   %{#{version} [#{date.to_time.strftime("%y%m%d.%H%M%S")}]}
+   %{#{rev} [#{date.strftime("%y%m%d.%H%M%S")}]}
  end
  
-end
+end  
